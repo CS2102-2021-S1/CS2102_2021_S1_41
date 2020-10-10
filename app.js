c3c24b2b20c59e73b7fce14928ad2d9a90f09144
Gi Hun Ko <rhrlgns96@gmail.com>
2020-10-11 02:02:51 +0800

    added initial project structure

--- app.js
+++ app.js
@@ -1,7 +1,25 @@
 const express = require('express');
-const app = express();
-const port = process.env.PORT || 3000;
+const bodyParser = require('body-parser');
+const path = require('path');
 
-app.get('/', (req, res) => res.send('Hello World!'));
+var users = require('./routes/users');
+//var routes = require('./routes');
+var app = express();
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.set('port', process.env.PORT || 5005);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static(path.join(__dirname, '../public')));
+
+//app.get('/', routes.index);
+app.get('/users', users.list);
+//app.get('/users/add', users.add);
+
+//app.post('/users/add', users.save);
+
+app.listen(app.settings.port, () => {
+   console.log(`Server is running on Port ${app.settings.port}. Press CTRL+C to stop server.`);
+});
