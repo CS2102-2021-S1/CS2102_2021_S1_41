6e3eae58e20fea68d7f4fbdc244800baf158b804
Gi Hun Ko <rhrlgns96@gmail.com>
2020-10-11 12:35:42 +0800

    added logging

--- routes/users.js
+++ routes/users.js
@@ -10,13 +10,14 @@ const client = new Client({
     connectionString: connectionString
 });
 client.connect();
+console.log(client);
 exports.list = (req, res) => {
     client.query("SELECT * FROM users", (err, result) => {
        if (err) {
            console.log(err);
            res.status(400).send(err);
        }
-       res.render("list", {title: "Users", data:result.rows});
+       res.render("users/list", {title: "Users", data:result.rows});
     });
 };
 
@@ -34,3 +35,16 @@ exports.edit = (req, res) => {
        res.render("users/edit", {title: "Edit Users", data: result.rows});
     });
 }
+
+exports.save = (req, res) => {
+    var cols = [req.body.login_name, req.body.uname, req.body.password_hash];
+    client.query("INSERT INTO users VALUES ($1, $2, $3)",
+        cols,
+        (err, result) => {
+            if (err) {
+                console.log("Error, user not saved! : %s", err);
+            }
+            res.redirect("/users");
+        }
+    );
+};
